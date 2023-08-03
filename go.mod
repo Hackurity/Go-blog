c081b9967dbd159813051fe68e950f73ec0b3f53
Hackurity <cucurity@yandex.ru>
2023-08-03 15:32:11 +0000

    Minimal working version

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/Hackurity/Go-blog
 
 go 1.20
+
+require (
+	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab
+	//github.com/scalingdata/go-martini-contrib-render v0.0.0-20161013161830-0c72f740a7ea
+)
+
+require (
+	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
+	github.com/scalingdata/go-inject v0.0.0-20161013160000-0503c2c471d4 // indirect
+	github.com/scalingdata/go-martini v0.0.0-20161013160822-524005143505 // indirect
+	github.com/scalingdata/go-oxtoacart-bpool v0.0.0-20161013171320-18f96869a170 // indirect
+)
--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+
+	"github.com/Hackurity/Go-blog/models"
+	"github.com/go-martini/martini"
 )
+
+var posts map[string]*models.Post
+var counter int
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
+	fmt.Println(counter)
 
-	t.ExecuteTemplate(w, "index", nil)
+	t.ExecuteTemplate(w, "index", posts)
 }
 
 func writeHandler(w http.ResponseWriter, r *http.Request) {
@@ -24,20 +31,72 @@ func writeHandler(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "write", nil)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	id := r.FormValue("id")
+	post, found := posts[id]
+	if !found {
+		http.NotFound(w, r)
+	}
+
+	t.ExecuteTemplate(w, "write", post)
+}
+
 func savePostHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.FormValue("id")
 	title := r.FormValue("title")
 	content := r.FormValue("content")
+
+	var post *models.Post
+	if id != "" {
+		post = posts[id]
+		post.Title = title
+		post.Content = content
+	} else {
+		id = GenerateId()
+		post := models.NewPost(id, title, content)
+		posts[post.Id] = post
+	}
+
+	http.Redirect(w, r, "/", 302)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	if id == "" {
+		http.NotFound(w, r)
+	}
+	delete(posts, id)
+	http.Redirect(w, r, "/", 302)
 }
 
 func main() {
 
-	fmt.Println("Jopa")
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/write", writeHandler)
-	http.HandleFunc("SavePost", savePostHandler)
+	fmt.Println("App is starting")
 
-	http.ListenAndServe(":3000", nil)
+	posts = make(map[string]*models.Post, 0)
+	counter = 0
+	m := martini.Classic()
+
+	m.Use(func(r *http.Request) {
+		if r.URL.Path == "/write" {
+			counter++
+		}
+	})
+
+	m.Get("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
+	staticOptions := martini.StaticOptions{Prefix: "assets"}
+	m.Use(martini.Static("assets", staticOptions))
+	m.Get("/", indexHandler)
+	m.Get("/write", writeHandler)
+	m.Post("/SavePost", savePostHandler)
+	m.Get("/edit", editHandler)
+	m.Get("/delete", deleteHandler)
+
+	m.Run()
 
 }
